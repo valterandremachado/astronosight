beef66fb825d2ec6ed484337961b9614aa05de56
Valter Machado <vmachado@johnsonsberkshire.com>
2022-03-21 16:55:15 +0800

    Initial Commit

---
+++ AstronoSight/AstronoSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronoSightApp.swift
+//  AstronoSight
+//
+//  Created by Valter A. Machado on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct AstronoSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
